e88b26aa8a9f2f2d198b2bbdae64b62b3adbf0fd
geqi <978606409@qq.com>
2018-12-28 11:00:52 +0800

    补充readme以及项目结构树

--- README.md
+++ README.md
@@ -1,21 +1,42 @@
-# todolist
+# VUE+Koa2+Mysql+JWT实现todolist
+----------
+## 项目说明：
+>作为我学习的练手项目，从0开始构建一个数据通过Koa2提供API的形式获取，页面通过Vue渲染的todolist。学习了Vue构建单页面的一些知识以及前端路由的使用、Koa如何提供API接口，如何进行访问过滤（路由）、验证（JSON-WEB-TOKEN）以及Sequelize操作MySQL数据库的一些知识和技巧
+---
+##项目使用的关键依赖：
+ >
 
-> A Vue.js project
+ - Vue
+ - Koa2
+ - axios 
+ - mysql
+ - sequelize
+ - jwt
+ - webpack
+ - scss
+---
+##使用方法：
+###git clone
+>git clone https://github.com/geqi1997/Todolist/tree/dev1
+### 1.1 安装
+>npm install
+### 1.2 配置数据库(打开/server/config/db.js）
+>修改如下const sequelize = new Sequelize('数据库', '数据库用户名', '数据库密码', {})
+### 1.3 项目启动
+>由于是前后端分离，所以分别用git 命令启动前台（npm run dev）和后台（node app.js）
+---
+##项目出现的问题及解决
+### 1.1 运用sequelize自动生成表结构
+>进入server的目录，执行如下语句sequelize-auto -o "./schema" -d todolist -h 127.0.0.1 -u root -p 3306 -x XXXXX -e mysql，（其中 -o 参数后面的是输出的文件夹目录， -d 参数后面的是数据库名， -h 参数后面是数据库地址， -u 参数后面是数据库用户名， -p 参数后面是端口号， -x 参数后面是数据库密码 -e 参数后面指定数据库为mysql）
+---
+>当时开开心心的生成了，后面在注册，生成TODO的时候一直报错，如下图:
+![图片描述][1]
+根据字面意思说的是TODO的Id不能为空，最后发现就是自动生成的表结构中没有 **autoIncrement: true** 
 
-## Build Setup
 
-``` bash
-# install dependencies
-npm install
-
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
-
-# build for production and view the bundle analyzer report
-npm run build --report
-```
-
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+  [1]: /img/bVblMLH  
+### 1.2 Koa2中注意use的位置
+>当在用Postman测试接口时，当返回是页面，或者是一条信息时就很凌乱，这个时候就需要检查一下不同use之间的顺序
+---
+##下阶段目标及总结
+>继续优化，改善界面。
--- app.js
+++ app.js
@@ -1,52 +1,52 @@
-const Koa=require('koa')
-const json=require('koa-json')
-const KoaRouter=require('koa-router')
-const auth=require('./server/routes/auth')
-const api=require('./server/routes/api')
-const jwt=require('koa-jwt')
-const path=require('path')
-const historyApiFallback=require('koa2-history-api-fallback')
-const serve=require('koa-static')
-const router=KoaRouter()
-const app=new Koa()
-app.use(require('koa-bodyparser')());
-app.use(json());
+const Koa = require('koa')
+const json = require('koa-json')
+const KoaRouter = require('koa-router')
+const auth = require('./server/routes/auth')
+const api = require('./server/routes/api')
+const jwt = require('koa-jwt')
+const path = require('path')
+const historyApiFallback = require('koa2-history-api-fallback')
+const serve = require('koa-static')
+const router = KoaRouter()
+const app = new Koa()
+app.use(require('koa-bodyparser')())
+app.use(json())
 
 app.use(async function (ctx, next) {
-    let start = new Date()
+  let start = new Date()
+  await next()
+  let ms = new Date() - start
+  console.log('%s %s - %s', ctx.method, ctx.url, ms)
+})
+
+app.use(async function (ctx, next) { //  如果JWT验证失败，返回验证失败信息
+  try {
     await next()
-    let ms = new Date() - start
-    console.log('%s %s - %s', ctx.method, ctx.url, ms)
-  })
-  
-  app.use(async function (ctx, next) {  //  如果JWT验证失败，返回验证失败信息
-    try {
-      await next()
-    } catch (err) {
-      if (err.status === 401) {
-        ctx.status = 401
-        ctx.body = {
-          success: false,
-          token: null,
-          info: 'Protected resource, use Authorization header to get access'
-        }
-      } else {
-        throw err
+  } catch (err) {
+    if (err.status === 401) {
+      ctx.status = 401
+      ctx.body = {
+        success: false,
+        token: null,
+        info: 'Protected resource, use Authorization header to get access'
       }
+    } else {
+      throw err
     }
-  })
-  
-  app.on('error', function (err, ctx) {
-    console.log('server error', err)
-  })
-  
-  router.use('/auth', auth.routes()) 
-  router.use('/api',jwt({secret: 'vue-koa-demo'}) ,api.routes()) // 所有从/api/打头的请求都需要经过jwt验证。
-  
-  app.use(router.routes())
-  app.use(historyApiFallback());//去掉history模式
-  app.use(serve(path.resolve('dist'))) // 将webpack打包好的项目目录作为Koa静态文件服务的目录
+  }
+})
+
+app.on('error', function (err, ctx) {
+  console.log('server error', err)
+})
+
+router.use('/auth', auth.routes())
+router.use('/api', jwt({ secret: 'vue-koa-demo' }), api.routes()) // 所有从/api/打头的请求都需要经过jwt验证。
+
+app.use(router.routes())
+app.use(historyApiFallback()) // 去掉history模式
+app.use(serve(path.resolve('dist'))) // 将webpack打包好的项目目录作为Koa静态文件服务的目录
 app.listen(3000, () => {
-    console.log('Koa is listening in 3000')
-  })
-module.exports = app;
+  console.log('Koa is listening in 3000')
+})
+module.exports = app
